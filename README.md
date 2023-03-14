98d9bff69ba7f886f8615114cb2c1edd69451138
Vlad Drozdov <drozdov.vl.1@yandex.ru>
2023-03-14 20:14:18 +0400

    feat: добавлены popup "Добавить карточку"; открытие на весь экран карточек; ставить like; удаление карточек.

--- README.md
+++ README.md
@@ -18,9 +18,14 @@
 * Позиционирование блоков и элементов.
 
 **JS**
-* Реализовано открытие PopUp при нажатии кнопки "Редактировать". И закрытие при клике на крестик в правом верхнем углу.
+* Реализовано открытие PopUp при нажатии кнопки "Редактировать", "Добавить" и нажатии на карточку. Закрытие при клике на крестик в правом верхнем углу.
 * Поля формы заполненны теми значениями, которые отображаются на странице.
 * Добавлена возможность редактирование имени и информации о себе.
+* Добавлена возможность ставить лайк.
+* Добавлена возможность удалять карточки.
+* Добавлена возможность открывать на весь экран карточки.
+
+
 
 ## Посмотреть проект
 **Сайт**
@@ -29,3 +34,38 @@
 
 **Figma**
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+
+# Project: Location
+**About the project**.
+* This project is an interactive service Mesto, where you can edit your profile, add photos, delete them and give likes.
+
+## Technology stack and functionality:
+
+**HTML**
+* Semantic tags;
+* Layout of the site using the layout from Figma;
+* Using Perfect Pixel.
+
+**CSS**
+**BEM Nested;
+* Grid Layout;
+* Flexbox;
+* Adaptive layout;
+* Use of media queries;
+* Positioning of blocks and elements.
+
+**JS**
+* Implemented opening of PopUp when you click "Edit", "Add" and click on the card. Closing by clicking on the cross in the upper right corner.
+* Form fields are filled with the values that are displayed on the page.
+* Added possibility to edit name and information about yourself.
+* Added possibility to give a nickname.
+* Added ability to remove cards.
+* Added ability to open cards in full screen.
+
+## View project
+**Site**
+* [Project Link](https://birddrozd.github.io/mesto/)
+
+
+**Figma**
+* [Link to Figma layout](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,132 @@
-const profileButtonEdit = document.querySelector(".profile__button-edit");
-const popup = document.querySelector(".popup");
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'Фото Архыза'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'Фото Челябинской области'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'Фото Иваново'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'Фото Камчатки'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'Фото Холмогорского район'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'Фото Байкала'
+  }
+];
 
-function openPopup() {
-  popup.classList.add("popup_opened");
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileAbout.textContent;
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const profileButtonEdit = document.querySelector('.profile__button-edit');
+const cardAddButton = document.querySelector('.profile__button-add');
+const elementsSection = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#element__template');
+const popupButtonClose = document.querySelectorAll('.popup__button-close');
+const popupEditProfile = document.querySelector('#profile');
+const popupEditButtonClose = popupEditProfile.querySelector('.popup__button-close');
+const popupEditNameInput = popupEditProfile.querySelector('.popup__input_value_bio-name');
+const popupEditAboutInput = popupEditProfile.querySelector('.popup__input_value_bio-about');
+const popupEditFormSave = popupEditProfile.querySelector('.popup__form');
+const popupElementAdd = document.querySelector('#element-add');
+const popupElementAddButtonClose = popupElementAdd.querySelector('.popup__button-close');
+const popupElementAddInputName = popupElementAdd.querySelector('.popup__input_value_place-name');
+const popupElementAddInputPic = popupElementAdd.querySelector('.popup__input_value_place-pic-url');
+const photoAddPopupFormSave = popupElementAdd.querySelector('.popup__form');
+const popupElementFullScreen = document.querySelector('#element-full-screen');
+const popupElementFullScreenButtonClose = popupElementFullScreen.querySelector('.popup__button-close');
+const popupElementFullScreenName = popupElementFullScreen.querySelector('.popup__element-name');
+const popupElementFullScreenImg = popupElementFullScreen.querySelector('.popup__element-img');
+
+initialCards.forEach((card) => {
+	const cardData = createCard(card);
+	elementsSection.append(cardData);	
+});
+
+function openPopup (popup) {
+	popup.classList.add('popup_opened');
 }
-
-const popupButtonClose = popup.querySelector(".popup__button-close");
-
-function closePopup() {
-  popup.classList.remove("popup_opened")
+function closePopup (popup) {
+	popup.classList.remove('popup_opened');
 }
-
-let nameInput = popup.querySelector(".popup__input_value_bio-name");
-let aboutInput = popup.querySelector(".popup__input_value_bio-about");
-let popupForm = popup.querySelector(".popup__form");
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__about");
-
-
-
-function submitForm(event) {
-  event.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileAbout.textContent = aboutInput.value;
-  closePopup()
+function createCard (card) {
+	const cardElement = cardTemplate.content.cloneNode(true);
+	const cardElementImg = cardElement.querySelector('.element__img');
+	cardElementImg.setAttribute('src', card.link);
+	cardElementImg.setAttribute('alt', card.alt);
+	cardElementImg.addEventListener('click', workElementFullScreen);
+	cardElement.querySelector('.element__name').textContent = card.name;
+	cardElement.querySelector('.element__waste-bin').addEventListener('click', workElementButtonDelete);
+	cardElement.querySelector('.element__button-like').addEventListener('click', workElementLike);
+	return cardElement;
+};
+function addCard (card) {
+	const newCard = createCard(card);
+	elementsSection.prepend(newCard);	
 }
+function workElementLike (like) {
+	like.target.classList.toggle('element__button-like_active');
+};
+function workElementButtonDelete (button) {
+	button.target.closest('.element__container').remove();
+};
+function workElementFullScreen (img) {
+	const actualImg = img.target.closest('.element__img');
+	const actualContainer = actualImg.closest('.element__container');
+	popupElementFullScreen.style = "background-color: rgba(0, 0, 0, 0.9)";
+	popupElementFullScreenImg.setAttribute('src', actualImg.src);
+	popupElementFullScreenImg.setAttribute('alt', actualImg.alt);
+	popupElementFullScreenName.textContent = actualContainer.querySelector('.element__name').textContent;
+	openPopup(popupElementFullScreen);
+};
 
-profileButtonEdit.addEventListener("click", openPopup);
-popupButtonClose.addEventListener("click", closePopup);
-popupForm.addEventListener("submit", submitForm);
+profileButtonEdit.addEventListener('click', function() {
+	openPopup(popupEditProfile);
+	popupEditNameInput.value = profileName.textContent;
+	popupEditAboutInput.value = profileAbout.textContent;
+});
+cardAddButton.addEventListener('click', function() {
+	openPopup(popupElementAdd);
+});
+
+popupButtonClose.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+popupEditFormSave.addEventListener('submit', function(event) {
+	event.preventDefault();
+	profileName.textContent = popupEditNameInput.value;
+	profileAbout.textContent = popupEditAboutInput.value;
+	closePopup(popupEditProfile);
+});
+
+photoAddPopupFormSave.addEventListener('submit', function(event) {
+	event.preventDefault();
+	const name = popupElementAddInputName.value;
+  const link = popupElementAddInputPic.value;
+	const alt = `Фото из ${name}`
+	const newCard = {
+		link,
+		name,
+		alt,
+	};
+	addCard(newCard);
+	event.target.reset();
+	closePopup(popupElementAdd);
+});
